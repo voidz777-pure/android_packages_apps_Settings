d64fdd6ed4e9c7e88c5d0f9b3c2dce6e5fb6f6a4
beanstown106 <nbenis106@gmail.com>
2015-10-18 14:03:15 -0400

    PureNexus about menu *g+, twitter, donate, pushbullet, contributors
    Change-Id: Id31f0718a4d8d607590388dd3f092a9c135e5f6d

---
+++ src/com/android/settings/nexus/about.java
@@ -0,0 +1,50 @@
+package com.android.settings.nexus;
+
+import android.content.ContentResolver;
+import android.database.ContentObserver;
+import android.net.Uri;
+import android.os.Bundle;
+import android.os.Handler;
+import android.preference.ListPreference;
+import android.preference.Preference;
+import android.preference.Preference.OnPreferenceChangeListener;
+import android.provider.Settings;
+import android.preference.PreferenceScreen;
+import android.preference.SwitchPreference;
+
+import java.util.Date;
+import com.android.internal.logging.MetricsLogger;
+
+import com.android.settings.R;
+import com.android.settings.SettingsPreferenceFragment;
+import com.android.settings.Utils;
+
+public class about extends SettingsPreferenceFragment implements
+        OnPreferenceChangeListener {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        addPreferencesFromResource(R.xml.nexus_about);
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+    }
+
+    public boolean onPreferenceChange(Preference preference, Object objValue) {
+    ContentResolver resolver = getActivity().getContentResolver();
+	return false;
+	}
+
+    public boolean onPreferenceTreeClick(PreferenceScreen preferenceScreen, Preference preference) {
+        return super.onPreferenceTreeClick(preferenceScreen, preference);
+    }
+
+    @Override
+    protected int getMetricsCategory() {
+        return MetricsLogger.APPLICATION;
+    }
+}
