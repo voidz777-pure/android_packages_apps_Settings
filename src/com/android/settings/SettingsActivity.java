b30984c91916b5788ab7fd14b5235089af0ca05e
beanstown106 <nbenis106@gmail.com>
2015-10-19 14:39:48 -0400

    Dashboard categories (SuperSU,LayersManager) *add SuperSu to rom control category *add layers manager to rom control category *make vectors for all and link to coloraccent like stock icons *make all depend on if the package is installed *make all multiuser friendly
    Change-Id: I41af881514f717b10b94560f536d83d64081a206
    
    Conflicts:
    	res/drawable/ic_settings_purenexus.xml
    	res/values/purenexus_strings.xml
    	res/xml/dashboard_categories.xml
    	src/com/android/settings/SettingsActivity.java

--- src/com/android/settings/SettingsActivity.java
+++ src/com/android/settings/SettingsActivity.java
@@ -283,7 +283,9 @@ public class SettingsActivity extends Activity
             R.id.nfc_payment_settings,
             R.id.home_settings,
             R.id.dashboard,
-            R.id.custom_settings
+            R.id.custom_settings,
+            R.id.bitsyko_layers,
+            R.id.supersu_settings
     };
 
     private static final String[] ENTRY_FRAGMENTS = {
@@ -1301,6 +1303,25 @@ public class SettingsActivity extends Activity
                     boolean hasPrintingSupport = getPackageManager().hasSystemFeature(
                             PackageManager.FEATURE_PRINTING);
                     if (!hasPrintingSupport) {
+                        removeTile = true;
+                    }
+                } else if (id == R.id.supersu_settings) {
+                    // Embedding into Settings is supported from SuperSU v1.85 and up
+                    boolean supported = false;
+                    try {
+                        supported = (getPackageManager().getPackageInfo("eu.chainfire.supersu", 0).versionCode >= 185);
+                    } catch (PackageManager.NameNotFoundException e) {
+                    }
+                    if (!supported) {
+                        removeTile = true;
+                    }
+                } else if (id == R.id.bitsyko_layers) {
+                    boolean supported = false;
+                    try {
+                        supported = (getPackageManager().getPackageInfo("com.lovejoy777.rroandlayersmanager", 0).versionCode > 0);
+                    } catch (PackageManager.NameNotFoundException e) {
+                    }
+                    if (!supported) {
                         removeTile = true;
                     }
                 } else if (id == R.id.development_settings) {
