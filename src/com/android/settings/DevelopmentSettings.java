b98a02ca0b6c3bd4c0aaaba57ce8b227c929cbd1
BeansTown106 <nbenis106@gmail.com>
2016-05-14 12:09:52 -0400

    Remove PowerMenu bugreport option *leave in tradditional bugreport as project fi users need it
    Change-Id: I252b2079b93b42f1eb6a0c2b0357f71d015e15ff

--- src/com/android/settings/DevelopmentSettings.java
+++ src/com/android/settings/DevelopmentSettings.java
@@ -121,7 +121,6 @@ public class DevelopmentSettings extends SettingsPreferenceFragment
     private static final String HARDWARE_UI_PROPERTY = "persist.sys.ui.hw";
     private static final String MSAA_PROPERTY = "debug.egl.force_msaa";
     private static final String BUGREPORT = "bugreport";
-    private static final String BUGREPORT_IN_POWER_KEY = "bugreport_in_power";
     private static final String OPENGL_TRACES_PROPERTY = "debug.egl.trace";
     private static final String TUNER_UI_KEY = "tuner_ui";
     private static final String COLOR_TEMPERATURE_PROPERTY = "persist.sys.debug.color_temp";
@@ -211,7 +210,6 @@ public class DevelopmentSettings extends SettingsPreferenceFragment
     private Preference mClearAdbKeys;
     private SwitchPreference mEnableTerminal;
     private Preference mBugreport;
-    private SwitchPreference mBugreportInPower;
     private SwitchPreference mKeepScreenOn;
     private SwitchPreference mBtHciSnoopLog;
     private SwitchPreference mEnableOemUnlock;
@@ -333,7 +331,6 @@ public class DevelopmentSettings extends SettingsPreferenceFragment
         }
 
         mBugreport = findPreference(BUGREPORT);
-        mBugreportInPower = findAndInitSwitchPref(BUGREPORT_IN_POWER_KEY);
         mKeepScreenOn = findAndInitSwitchPref(KEEP_SCREEN_ON);
         mBtHciSnoopLog = findAndInitSwitchPref(BT_HCI_SNOOP_LOG);
         mEnableOemUnlock = findAndInitSwitchPref(ENABLE_OEM_UNLOCK);
@@ -615,8 +612,6 @@ public class DevelopmentSettings extends SettingsPreferenceFragment
                     context.getPackageManager().getApplicationEnabledSetting(TERMINAL_APP_PACKAGE)
                             == PackageManager.COMPONENT_ENABLED_STATE_ENABLED);
         }
-        updateSwitchPreference(mBugreportInPower, Settings.Secure.getInt(cr,
-                Settings.Secure.BUGREPORT_IN_POWER_MENU, 0) != 0);
         updateSwitchPreference(mKeepScreenOn, Settings.Global.getInt(cr,
                 Settings.Global.STAY_ON_WHILE_PLUGGED_IN, 0) != 0);
         updateSwitchPreference(mBtHciSnoopLog, Settings.Secure.getInt(cr,
@@ -930,21 +925,16 @@ public class DevelopmentSettings extends SettingsPreferenceFragment
                     resolver, Settings.Global.ADB_ENABLED, 0) != 0;
             if (adbEnabled) {
                 mBugreport.setEnabled(true);
-                mBugreportInPower.setEnabled(true);
                 getPackageManager().setComponentEnabledSetting(
                         bugreportStorageProviderComponentName,
                         PackageManager.COMPONENT_ENABLED_STATE_ENABLED, 0);
             } else {
                 mBugreport.setEnabled(false);
-                mBugreportInPower.setEnabled(false);
-                mBugreportInPower.setChecked(false);
-                Settings.Secure.putInt(resolver, Settings.Secure.BUGREPORT_IN_POWER_MENU, 0);
                 getPackageManager().setComponentEnabledSetting(
                         bugreportStorageProviderComponentName,
                         PackageManager.COMPONENT_ENABLED_STATE_DEFAULT, 0);
             }
         } else {
-            mBugreportInPower.setEnabled(true);
             getPackageManager().setComponentEnabledSetting(
                     bugreportStorageProviderComponentName,
                     PackageManager.COMPONENT_ENABLED_STATE_ENABLED, 0);
@@ -1729,10 +1719,6 @@ public class DevelopmentSettings extends SettingsPreferenceFragment
             pm.setApplicationEnabledSetting(TERMINAL_APP_PACKAGE,
                     mEnableTerminal.isChecked() ? PackageManager.COMPONENT_ENABLED_STATE_ENABLED
                             : PackageManager.COMPONENT_ENABLED_STATE_DEFAULT, 0);
-        } else if (preference == mBugreportInPower) {
-            Settings.Secure.putInt(getActivity().getContentResolver(),
-                    Settings.Secure.BUGREPORT_IN_POWER_MENU,
-                    mBugreportInPower.isChecked() ? 1 : 0);
         } else if (preference == mKeepScreenOn) {
             Settings.Global.putInt(getActivity().getContentResolver(),
                     Settings.Global.STAY_ON_WHILE_PLUGGED_IN,
